085a8e2691c78b7ffc76915b1a274f8cb819a176
Arkadiusz Sidzinka <arkadiusz.sidzinka@gmail.com>
2022-04-09 22:55:14 +0200

    remove unused imports

--- src/app.dart
+++ src/app.dart
@@ -1,6 +1,3 @@
-// import 'dart:async' show Future;
-// import 'package:shelf_router/shelf_router.dart';
-// import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'controllers/MainController.dart';
 import './modules/note/repositories/NoteRepository.dart';
@@ -9,10 +6,9 @@ import 'lib/DB.dart';
 import 'modules/note/services/NoteService.dart';
 
 void main() async {
-  // Variable for PORT
-
   final db = DB();
   await db.initConnection();
+  
   final noteRepository = NoteRepository(db);
   final noteService = NoteService(noteRepository);
   final notesController = NotesController(noteService);
